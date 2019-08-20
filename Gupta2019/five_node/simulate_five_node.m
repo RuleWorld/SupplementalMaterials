17ed43287a8f23b303467516e39272700693c876
Sanjana Gupta <sag134@pitt.edu>
2019-08-20 19:13:33 -0400

    nfkb convergence files

--- Gupta2019/five_node/simulate_five_node.m
+++ Gupta2019/five_node/simulate_five_node.m
@@ -1,18 +1,4 @@
 function [err, sp, obsv] = simulate_five_node(t,init,params)
-%SIMULATE Generate trajectory for Michaelis-Menten
-%
-%  [err, sp, obsv] = simulate( t, init, params, S0, E0, cfg )
-%
-%  generate Michaelis-Menten trajectory, where 't' is a vector
-%  of time points, with parameters 'params', with initial substrate 'S0',
-%  initial enzyme 'S0', and additional configuration arguments 'cfg'.
-%
-%  If 'init' = [], then default initial conditions are used. Note that
-%  initial conditions are with respect to the first time point, not
-%  necessarily t=0.  
-%
-%  Time units are seconds. The state trajectories and observables 
-%  have concentration units
 %%
 t = linspace(0,50,8)';
 % run simulation
