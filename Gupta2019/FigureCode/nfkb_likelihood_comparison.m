083c0001da3237b3eb21e76570798020a29b4d14
Sanjana Gupta <sag134@pitt.edu>
2019-09-03 23:58:28 -0400

    updated and added nfkb figure code

--- Gupta2019/FigureCode/nfkb_likelihood_comparison.m
+++ Gupta2019/FigureCode/nfkb_likelihood_comparison.m
@@ -2,70 +2,80 @@
 addpath('/shared2/LabUserFiles/Sanjana_Gupta/Original/ptempest/core/');
 % path to supplementary distributions
 addpath('/shared2/LabUserFiles/Sanjana_Gupta/Original/ptempest/core/distr/');
-addpath('/shared2/LabUserFiles/Sanjana_Gupta/LassoManuscript/lib')
-addpath('/shared2/LabUserFiles/Sanjana_Gupta/LassoManuscript/lib/aboxplot')
-range = 5e4:1e3:9.9e5-1;
-e = zeros(2,length(range)*6,3);
-h = ones(5,1);
-p = ones(5,1);
+addpath('../lib')
+addpath('../lib/aboxplot')
+range = 5e4:1e3:9.9e5-1; %number of points to simulate to get likelihood distribution
+e = zeros(2,length(range)*6,3);% array to store likelihood values. the first index is for with or without lasso. The second index corresponds to each parameter set tested. The third index corresponds to the NFkB trajectory
 mu = -25;
 b = 2;
 repeat = 1;
 counter=1;
 trajectory_counter=1;
-for trajectory_number = 2:4%2:4
+for trajectory_number = 2:4 % these are the trajectories for which the fits converged   
     c = 1; 
-    energy_tmp = zeros(6,length(range));
+    energy_tmp = zeros(6,length(range)); % energy_tmp collects the likelihood values for a particular trajectory (all repeats)
     repeat_counter=1;
+    %alternate repeats correspond to the same initial condition and are
+    %combined
     for repeat = 1:2:12
         data = load(['../SingleCellNFkB_pulse/SingleCellNFkB_continue_chain_reduced_model_fixedstart_withlasso_mu_',num2str(mu),'_b_',num2str(b),'_trajectory_',num2str(trajectory_number),'_repeat_',num2str(repeat),'_progress990000.mat']);                     
+        %Checking to make sure the initial parameter set is the same
+        if repeat_counter==1
+            tmp_init = data.cfg.initp;
+        else
+            if sum(sum(tmp_init~=data.cfg.initp))~=0
+                disp('problem');
+                return
+            else 
+                tmp_init = data.cfg.initp;
+            end
+        end   
         counter=1;
-        
         for i = range
             i
-            params = data.params_chain(1,:,i);
+            params = data.params_chain(1,:,i); 
+            %calculate likelihood of each parameter set in the specified
+            %range. in ptempest energy = liklihood - logprior. so now
+            %likliehood  = energy + logprior
             energy_tmp(repeat_counter,counter) = data.energy_chain(1,i) + data.cfg.logpdf_prior_fcn(params);%likelihood(params,data1.cfg); 
             counter=counter+1;
         end
         repeat_counter=repeat_counter+1;
-        
     end
+    %Note that the (:) operatore concatenates columns. 
+    %e(2,1,1) will correspond to energy_chain(1,range(1)) of trajectory 1, repeat 1
+    %e(2,2,1) will correspond to energy_chain(1,range(1)) of trajectory 1, repeat 2
+    %e(2,7,1) will correspond to energy_chain(1,range(2)) of trajectory 1, repeat 1
     e(2,:,trajectory_counter) = energy_tmp(:);
     %%
     energy_tmp1 = zeros(6,length(range));
     repeat_counter=1;
-    for repeat = 2:2:12 
+    for repeat = 1:2:12 % for biorxiv this was set to 2:2:12. Doesn't make a difference (since converged) but changed to 1:2:12 for consistency.
         repeat
-        data2 = load(['../SingleCellNFkB_pulse/SingleCellNFkB_continue_chain_wolasso_fixedstart_trajectory_',num2str(trajectory_number),'_repeat_',num2str(repeat),'_progress990000.mat']);
+        data2 = load(['../SingleCellNFkB_pulse/SingleCellNFkB_continue_chain_wolasso_fixedstart_trajectory_',num2str(trajectory_number),'_repeat_',num2str(repeat),'_progress990000.mat']);       
+        %Checking to make sure the initial parameter set is the same
+        if repeat_counter==1
+            tmp_init = data2.cfg.initp;
+        else
+            if sum(sum(tmp_init~=data2.cfg.initp))~=0
+                disp('problem');
+                return
+            else 
+                tmp_init = data2.cfg.initp;
+            end
+        end 
+        % without lasso there is just a uniform prior and the energy = the
+        % liklihood
         energy_tmp1(repeat_counter,:) = data2.energy_chain(1,range);
         repeat_counter=repeat_counter+1;
     end
-    e(1,:,trajectory_counter) = energy_tmp1(:);
-    trajectory_counter = trajectory_counter+1;
-    
+    %Note that the (:) operatore concatenates columns. 
+    %e(1,1,1) will correspond to energy_chain(1,range(1)) of trajectory 1, repeat 1
+    %e(1,2,1) will correspond to energy_chain(1,range(1)) of trajectory 1, repeat 2.
+    %e(1,7,1) will correspond to energy_chain(1,range(2)) of trajectory 1, repeat 1
+    e(1,:,trajectory_counter) = energy_tmp1(:); 
+    trajectory_counter = trajectory_counter+1;    
 end
 %%
 f = figure;aboxplot(e,'OutlierMarker','none');
-
-for i = 1:3
-    e1 =e(1,:,i);
-    e2 = e(2,:,i);
-    
-    [p,h] = ranksum(e(1,:,i),e(2,:,i))
-    
-    if h==1
-        [q1 q2 q3 fu1 fl1 ou ol] = quartile(e1);
-        [q1 q2 q3 fu2 fl2 ou ol] = quartile(e2);
-        hold on
-        plot([(i-1)+0.825,(i-1)+1.175],[fu1,fu1]+1.5,'LineWidth',2,'Color',[0.5,0.5,0.5])
-        hold on
-        plot([(i-1)+0.825,(i-1)+0.825],[fu1+0.2,fu1+1.5],'LineWidth',2,'Color',[0.5,0.5,0.5])
-        plot([(i-1)+1.175,(i-1)+1.175],[fu2+0.2,fu1+1.5],'LineWidth',2,'Color',[0.5,0.5,0.5])
-        hold on
-        scatter((i-1)+0.825+(1.175-0.825)/2,fu1+2.5,150,'*','MarkerEdgeColor',[0.3,0.3,0.3])
-    end
-end
-
-toPDF(f,'pdf_updated_figures_July23_2019/Figure4c_likelihoodcomparison.pdf')
-%toPDF(f,'pdf_figures/Figure4c_likelihoodcomparison.pdf')
-%savefig('NFkBErrorFigure','png','-c0','-crop')
+toPDF(f,'nfkb_updated_figures_Sept3/Figure4c_likelihoodcomparison_Sept2.pdf')
--- Gupta2019/FigureCode/supplementary_figure_nfkb_model_predictions.m
+++ Gupta2019/FigureCode/supplementary_figure_nfkb_model_predictions.m
@@ -4,12 +4,12 @@ addpath('/shared2/LabUserFiles/Sanjana_Gupta/Original/ptempest/core/');
 addpath('/shared2/LabUserFiles/Sanjana_Gupta/Original/ptempest/core/distr/');
 addpath('../lib');
 counter=1;
-LB = 5e4;
+LB = 5e4; %for biorxiv version this was set to 1e4. Changing it to 5e4 for consistency with other figures
 UB = 9.9e5;
-step = 5e2;
+step = 5e2;%number of samples per repeat. Total number of parameters sampled here = 5e2*
 mu = -25;
 b = 2;
-repeat = 2;
+repeat = 1;
 trajectory_number = 2; %a representative trajectory
 tequil = (0:1e5:1e8)';
 tstim = (0:1:5*60)';
@@ -41,3 +41,12 @@ for j = round(linspace(LB,UB,step))
     end
     hold on
 end
+
+for i = 1:12
+    subplot(3,4,i);
+    yy = ylim;
+    yy(1) = 0;
+    yy
+    ylim(yy);
+end
+toPDF(f2,'nfkb_updated_figures_Sept3/nfkb_model_prediction_sept2.pdf');
